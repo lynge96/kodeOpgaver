0aae883808edc9d33bbfe64d1625e45cbb9fa6cf
Anders <90785699+lynge96@users.noreply.github.com>
2022-02-23 11:50:01 +0100

    finpudsning

--- 2_semester_CS/modul3_opg/opg3.06/renteRegning.cs
+++ 2_semester_CS/modul3_opg/opg3.06/renteRegning.cs
@@ -6,15 +6,15 @@
 
 double balance = 10000;
 double rente = 1.05;
-for (double i = 0; i < 20; i++)
+for (double i = 1; i < 20; i++)
 {
     // Lægger renten til den nye balance hvert år.
     balance = balance * rente;
-    Console.WriteLine($"År {i} Balance: {balance}");
+    Console.WriteLine($"År: {i} - Balance: {Math.Round(balance, 2)}");
     // Hvis balancen er over 20000, stopper loopet, og den printer året + beløbet.
     if (balance >= 20000)
     {
-        Console.WriteLine($"Formuen er fordoblet efter {i} år. Balancen er på {balance}");
+        Console.WriteLine($"Formuen er fordoblet efter {i} år.\nBalancen er på {Math.Round(balance, 2)}");
         break;
     }
 }

3648d8059479510f868e88aae140e2571892c5c2
Anders <90785699+lynge96@users.noreply.github.com>
2022-03-17 10:54:52 +0100

    2darray

---
+++ 2_semester_CS/modul8_opg/opg8.05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Ekstra 8.05 - To-dimensionelle arrays
+
+// I en konsol-applikation, lav en to-dimensionel array med strenge. Den skal indeholde to rækker og tre kolonner. Udskriv i konsollen værdien i den anden række og den tredje kolonne.
+
+// Opretter et to-dimensionelt array:
+String[,] array2d;
+// 2 rækker, 3 kolonner.
+array2d = new String[2, 3] { { "Anders", "Lynge", "Ravnsbæk" }, { "Rikke", "Schouby", "Hein" } };
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Printer to-dimensionelt array:");
+Console.ResetColor();
+// Udskriver hele arrayet.
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (j == 2)
+        {
+            Console.Write($"{array2d[i, j]}\n");
+        }
+        else
+        {
+            Console.Write($"{array2d[i, j]} ");
+        }
+    }
+}
+// Udskriver [2,3], anden række og tredje kolonne:
+Console.WriteLine("-----------------------------");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine($"Udskriver [1, 2]:");
+Console.ResetColor();
+Console.WriteLine($"{array2d[1, 2]}");

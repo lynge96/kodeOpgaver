48d51c0604cecced38aa6db24cd11d04b0fe6d79
Anders <90785699+lynge96@users.noreply.github.com>
2022-04-21 11:57:24 +0200

    trim

--- 2_semester_CS/modul16_opg/modul16_opg/Mekaniker.cs
+++ 2_semester_CS/modul16_opg/modul16_opg/Mekaniker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace opg16._02
+﻿namespace opg16._02
 {
     public class Mekaniker : Person
     {

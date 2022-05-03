3a1c50b234035a6477c186f3c1201d6a8046809c
Anders <90785699+lynge96@users.noreply.github.com>
2022-05-03 16:01:42 +0200

    formsAndValidation

---
+++ 2_semester_CS/modul19_opg/modul19_opg/Client/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using modul19_opg.Shared.Models;
+
+namespace modul19_opg.Client.Pages
+{
+    public partial class Index
+    {
+        private Product ProductModel = new Product();
+        private EditContext EditContext;
+
+        protected override void OnInitialized()
+        {
+            EditContext = new EditContext(ProductModel);
+        }
+
+        private void HandleSubmit()
+        {
+            Console.WriteLine("HandleSubmit Called...");
+
+            if (EditContext.Validate())
+            {
+                Console.WriteLine("Form is Valid...");
+            }
+            else
+            {
+                Console.WriteLine("Form is Invalid...");
+            }
+        }
+    }
+
+}
---
+++ 2_semester_CS/modul19_opg/modul19_opg/Shared/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace modul19_opg.Shared.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime PublishedDate { get; set; } = DateTime.UtcNow;
+    }
+}

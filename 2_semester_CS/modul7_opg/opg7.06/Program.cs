8d95355cf2eecb15ce8beb822ab953a356116e91
Anders <90785699+lynge96@users.noreply.github.com>
2022-05-03 20:00:16 +0200

    ekstra opgaver

---
+++ 2_semester_CS/modul7_opg/opg7.06/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hvor mange minutter?");
+int minutterInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{minutterInput} minutter er {AntalSekunder(minutterInput)} sekunder\n");
+
+// a)
+static int AntalSekunder(int antalMinutter)
+{
+    int antalSekunder = antalMinutter * 60;
+
+    return antalSekunder;
+}
+
+
+Console.WriteLine("Hvor gammel er du?");
+int alderInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{alderInput} år er det samme som {AlderTilDage(alderInput)} dage\n");
+
+// b)
+static int AlderTilDage(int alder)
+{
+    int antalDage = alder * 365;
+
+    return antalDage;
+}
+
+
+Console.WriteLine("Er dit tal mindre en 0?");
+int talInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Er tallet {talInput} mindre end nul?: {MindreEndNul(talInput)}");
+
+// c)
+static bool MindreEndNul(int tal)
+{
+    if (tal < 0)
+    {
+        return true;
+    }
+    else return false;
+}

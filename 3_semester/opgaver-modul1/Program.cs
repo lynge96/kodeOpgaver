08b250851169d813547521dd26bd62e0ed8b4012
Anders <90785699+lynge96@users.noreply.github.com>
2022-08-29 14:45:09 +0200

    opgave4

--- 3_semester/opgaver-modul1/Program.cs
+++ 3_semester/opgaver-modul1/Program.cs
@@ -2,6 +2,9 @@
 Console.WriteLine(Opgave3.Faculty(5)); // Output skal være '120'.
 Console.WriteLine(Opgave4.sfd(20, 15));
 Console.WriteLine(Opgave4.nPotens(5, 3));
+Console.WriteLine(Opgave4.GangeToTal(5, 4));
+Console.WriteLine(Opgave4.Reverse("EGAKNANAB"));
+Opgave5.ScanDir("C:\\Users\\lynge\\Desktop\\kodeOpgaver");
 
 // Opgave 3 - Beregn fakultet med rekursion
 class Opgave3 {
@@ -40,4 +43,44 @@ class Opgave4 {
     }
 
     // Delopgave 3
+    public static int GangeToTal(int a, int b) {
+        int result;
+
+        if (b == 0) {
+            result = 0;
+        } else {
+            result = a + GangeToTal(a, b - 1);
+        }
+        return result;
+    }
+
+    //Delopgave 4
+    public static string Reverse(string ord) {
+
+        if (ord.Length == 0) {
+            return ord;
+        }
+        return Reverse(ord.Substring(1)) + ord[0];
+    }
+}
+// Opgave 5 - Gennemlæsning af mappe på disken
+class Opgave5 {
+    public static void ScanDir(string path)
+    {
+        DirectoryInfo dir = new DirectoryInfo(path);
+        FileInfo[] files = dir.GetFiles();
+
+        // Udskriver alle filerne
+        foreach (FileInfo file in files)
+        {
+            Console.WriteLine(file.Name);
+        }
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Kalder rekursivt på alle undermapper
+        foreach (DirectoryInfo subdir in dirs)
+        {
+            ScanDir(subdir.FullName);
+        }
+    }
 }
